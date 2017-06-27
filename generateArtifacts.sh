acaa1c43ade0795482b2b5784def3168b208a43a
surya <suryalnvs@gmail.com>
2017-06-27 20:35:00 +0000

    fixing generateArtifacts

--- generateArtifacts.sh
+++ generateArtifacts.sh
@@ -38,7 +38,7 @@ function replacePrivateKey () {
 
 ## Generates Org certs using cryptogen tool
 function generateCerts (){
-	CRYPTOGEN=../build/bin/cryptogen
+	CRYPTOGEN=$FABRIC_ROOT/release/$OS_ARCH/bin/cryptogen
 
 	if [ -f "$CRYPTOGEN" ]; then
             echo "Using cryptogen -> $CRYPTOGEN"
@@ -58,7 +58,7 @@ function generateCerts (){
 ## Generate orderer genesis block , channel configuration transaction and anchor peer update transactions
 function generateChannelArtifacts() {
 
-	CONFIGTXGEN=../build/bin/configtxgen
+	CONFIGTXGEN=$FABRIC_ROOT/release/$OS_ARCH/bin/configtxgen
 	if [ -f "$CONFIGTXGEN" ]; then
             echo "Using configtxgen -> $CONFIGTXGEN"
 	else
